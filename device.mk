05e65b8e7fb705be06bcc8bd3c477bc97bd649ee
bengris32 <bengris32@protonmail.ch>
2023-11-13 16:12:47 +0000

    corot: Inherit `generic_ramdisk` product configuration
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I01fb5cd9a7e1f461848bcabd8874aed9f841cbac

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit generic_ramdisk product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
