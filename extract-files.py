8e1da4179cce89bc1dc6f66b634935eca74583df
alextrack2013 <alextrack2013@gmail.com>
2024-03-30 21:31:20 +0000

    corot: Link Audio HAL against stock `libalsautils`
    * Fixes audio playback in some cases with USB headphones.
    
    Change-Id: I4d904b1610dc6e17f29546f6f7f4f04bac0ca4c3
    Signed-off-by: alextrack2013 <alextrack2013@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -33,7 +33,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/android.hardware.security.keymint@2.0-service.mitee': blob_fixup()
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so'),
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libalsautils.so', 'libalsautils-v33.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
