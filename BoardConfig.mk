a816d43d1b9aca5d8bb5e7fe2c849b3bb796d98e
ChrisCatto <chriscatto@proton.me>
2025-01-22 18:33:47 +0200

    corot: Define Architecture flags
    Change-Id: I3ace1a19a92671f07d605cfb44803f73f3819ccd
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,11 @@
 
 DEVICE_PATH := device/xiaomi/corot
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv9-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
 # Inherit the proprietary files
 include vendor/xiaomi/corot/BoardConfigVendor.mk
