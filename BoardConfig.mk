2f1b2d45854808fe21ab0cfb1f2fdbad66fa6360
ChrisCatto <chriscatto@proton.me>
2025-01-22 18:36:15 +0200

    corot: Define Bootloader and Platform flags
    Change-Id: I4b94e9f8db8b23e666eae7c6b382023c47faf62d
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,12 @@ TARGET_ARCH_VARIANT := armv9-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a76
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := corot
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6985
+
 # Inherit the proprietary files
 include vendor/xiaomi/corot/BoardConfigVendor.mk
