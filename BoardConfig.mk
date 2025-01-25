1673c96d72a0aafa48c3e9ddcf65c8f94a93b36f
ChrisCatto <chriscatto@proton.me>
2025-01-25 23:29:53 +0200

    corot: Define Recovery flags
    Change-Id: I6d9157a4782f867a9848786b39212ad1923a2832
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,5 +126,10 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 # Platform
 TARGET_BOARD_PLATFORM := mt6985
 
+# Recovery
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+TARGET_USERIMAGES_USE_F2FS := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/corot/BoardConfigVendor.mk
