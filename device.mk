6cd69c8b195ac6ace265ee67cc75b6a54a330cb8
ChrisCatto <chriscatto@proton.me>
2025-01-26 09:46:44 +0200

    corot: Build AOSP AIDL Boot HAL
    Change-Id: I3f33084918969ceec7fb3976522c8ffa41ffc8d3
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 PRODUCT_PACKAGES += \
+    android.hardware.boot-service.default \
+    android.hardware.boot-service.default_recovery
+
+PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
