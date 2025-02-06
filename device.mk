de32c3533386bbacfab776701b784a7a9e377df0
ChrisCatto <chriscatto@proton.me>
2025-02-06 18:42:02 +0200

    corot: Build common Light service
    Change-Id: I2c16e4b7565adbf5b798b3009e55964cfc6e930f
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- device.mk
+++ device.mk
@@ -178,6 +178,10 @@ PRODUCT_PACKAGES += \
     libcppbor_external.vendor \
     libkeymint.vendor
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lineage
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
