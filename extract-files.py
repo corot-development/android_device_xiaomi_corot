5dc12e99d2620a40ecf8111cae5a1a4e0b7f2ade
ChrisCatto <chriscatto@proton.me>
2025-03-13 17:54:55 +0200

    corot: Fix elf checks for `libimsma`
    Change-Id: I3051e4e4b191c2f77c37cb9ecadaa4530b25b975
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -38,8 +38,10 @@ blob_fixups: blob_fixups_user_type = {
         .apktool_patch('blob-patches/ImsService.patch', '-r'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
-    'system_ext/lib64/libsink.so': blob_fixup()
+    'system_ext/lib64/libsink-mtk.so': blob_fixup()
         .add_needed('libaudioclient_shim.so'),
+    'system_ext/lib64/libimsma.so': blob_fixup()
+         .replace_needed('libsink.so', 'libsink-mtk.so'),
     ('system_ext/etc/init/init.vtservice.rc', 'vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc'): blob_fixup()
         .regex_replace('start', 'enable'),
     'vendor/bin/hw/android.hardware.graphics.composer@3.1-service': blob_fixup()
